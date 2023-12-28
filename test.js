2aeff1a4ddeaf6308c7cd6e8af93676ce200126d
GreicodexJM <64851393+GreicodexJM@users.noreply.github.com>
2023-12-28 18:50:55 -0400

    stop reentry

--- test.js
+++ test.js
@@ -1,9 +1,12 @@
 console.log("Script loaded");
-var col=document.getElementsByClassName("kid-birthday")[0].getElementsByClassName("block");
-for(var i =0;i<col.length;i++) {
-    var e = col.item(e);
-    var c=document.createElement("div"); 
-    c.className="field is-floating-label"; 
-    c.innerHTML='<label class="label">Coupon</label><div class="control is-clearfix"><input type="text" id="coupon_number" autocomplete="on" required="required" class="input"></div>';
-    e.append(c);
+if(null == window.scriptLoadedHowndProof) {
+    var col=document.getElementsByClassName("kid-birthday")[0].getElementsByClassName("block");
+    for(var i =0;i<col.length;i++) {
+        var e = col.item(e);
+        var c=document.createElement("div"); 
+        c.className="field is-floating-label"; 
+        c.innerHTML='<label class="label">Coupon</label><div class="control is-clearfix"><input type="text" id="coupon_number" autocomplete="on" required="required" class="input"></div>';
+        e.append(c);
+    }
+    window.scriptLoadedHowndProof=true;
 }
