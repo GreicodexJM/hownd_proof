7b6ca65ea64a52cb02f202d76e5a5b0d8012ef24
GreicodexJM <64851393+GreicodexJM@users.noreply.github.com>
2023-12-29 13:16:27 -0400

    event listener

--- test.js
+++ test.js
@@ -1,12 +1,18 @@
 console.log("Script loaded");
 if(null == window.scriptLoadedHowndProof) {
-    var col=document.getElementsByClassName("kid-birthday")[0].getElementsByClassName("block");
-    for(var i =0;i<col.length;i++) {
-        var e = col.item(e);
-        var c=document.createElement("div"); 
-        c.className="field is-floating-label"; 
-        c.innerHTML='<label class="label">Coupon</label><div class="control is-clearfix"><input type="text" id="coupon_number" autocomplete="on" required="required" class="input"></div>';
-        e.append(c);
+    function fn() {
+        var col=document.getElementsByClassName("kid-birthday")[0].getElementsByClassName("block");
+        for(var i =0;i<col.length;i++) {
+            var e = col.item(e);
+            if(0==e.getElementsByClassName("coupon_num").length) {
+                var c=document.createElement("div"); 
+                c.className="field is-floating-label"; 
+                c.innerHTML='<label class="label">Coupon</label><div class="control is-clearfix coupon_num"><input type="text" id="coupon_number" autocomplete="on" required="required" class="input"></div>';
+                e.append(c);
+            }
+        }
     }
+    var a = document.getElementsByClassName('linkOverride');
+    a[1].addEventListener('click',fn);
     window.scriptLoadedHowndProof=true;
 }
